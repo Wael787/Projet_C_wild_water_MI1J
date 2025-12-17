532536fcab1e79bdd7ef9deef5e7d6f54de21c30
Wael787 <wael.wadih06@gmail.com>
2025-12-17 20:41:42 +0100

    Update histo.h

--- includes/histo.h
+++ includes/histo.h
@@ -7,16 +7,15 @@
 #include <math.h>
 #include <errno.h>
 
-#define MAX_LINE 4096
-#define KILO 1000.0 // Pour la conversion m³ -> k.m³
+#define MAX_LINE 4096   // Taille maximale d'une ligne dans le fichier CSV
+#define KILO 1000.0     // Facteur de conversion pour m³ en k.m³
 
 // STRUCTURE DE L'ARBRE AVL (Usine)
 
 typedef struct arbre_usine {
     char* id;              
-    long long vol_max;     
-    long long vol_capte;   
-    long long vol_traite;  
+    long long vol_capte;   // Pour'src'
+    long long vol_traite;  // Pour 'real'
     int hauteur;           
     struct arbre_usine* fg; 
     struct arbre_usine* fd; 
@@ -24,19 +23,22 @@ typedef struct arbre_usine {
 
 typedef Usine* pUsine;
 
+// PROTOTYPES DES FONCTIONS PUBLIQUES
 
+pUsine creerUsine(const char* id, long long vol_capte, long long vol_traite);
 
-// Fonctions AVL
-pUsine creerUsine(const char* id, long long vol_max, long long vol_capte, long long vol_traite);
 pUsine insertUsine(pUsine racine, pUsine nouvelle_data);
-void libererAVL(pUsine racine);
-void parcoursInfixeInverse(pUsine racine, FILE* fichier, const char* type);
 
+void libererAVL(pUsine racine);
+
+void parcoursInfixeInverse(pUsine racine, FILE* fichier, const char* type);
 
 void traiterLigne(char* ligne, pUsine* racine);
 
-// Fonctions de Conversion
+// Prototypes fonctions utilitaires de conversion
+
 long long parseLongLong(const char* str);
 double parseDouble(const char* str);
+
 
 #endif
