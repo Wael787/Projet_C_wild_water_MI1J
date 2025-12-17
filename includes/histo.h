753a3cf1ebc8a691fbb1fe843a7c1b8fe4317b68
Wael787 <wael.wadih06@gmail.com>
2025-12-17 10:43:23 +0100

    Create histo.h

---
+++ includes/histo.h
@@ -0,0 +1,10 @@
+#ifndef HISTO_H
+#define HISTO_H
+
+#include "codes_erreur.h"
+
+// Prototype de la fonction de traitement pour la commande 'histo' (histogramme).
+ 
+int traiter_histo(int argc, char *argv[]);
+
+#endif
