e361d8ebc643133b1b77f3ae58a87cbf184c36db
Wael787 <wael.wadih06@gmail.com>
2025-12-20 18:14:09 +0100

    Update histo.h

--- includes/histo.h
+++ includes/histo.h
@@ -4,41 +4,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <errno.h>
 
-#define MAX_LINE 4096   // Taille maximale d'une ligne dans le fichier CSV
-#define KILO 1000.0     // Facteur de conversion pour m³ en k.m³
+#define MAX_LINE 2048
+#define MAX_ENTRIES 1000
 
-// STRUCTURE DE L'ARBRE AVL (Usine)
+// Structure pour stocker les données
+typedef struct {
+    char nom[300];
+    double volume;
+} Donnee;
 
-typedef struct arbre_usine {
-    char* id;              
-    long long vol_capte;   // Pour'src'
-    long long vol_traite;  // Pour 'real'
-    int hauteur;           
-    struct arbre_usine* fg; 
-    struct arbre_usine* fd; 
-} Usine;
+// Variables globales
+extern Donnee tab[MAX_ENTRIES];
+extern int nb;
 
-typedef Usine* pUsine;
+// Fonctions utilitaires
+void nettoyer(char* s);
+int chercher(char* nom);
+void ajouter(char* nom, double vol);
+int comparer(const void* a, const void* b);
 
-// PROTOTYPES DES FONCTIONS PUBLIQUES
-
-pUsine creerUsine(const char* id, long long vol_capte, long long vol_traite);
-
-pUsine insertUsine(pUsine racine, pUsine nouvelle_data);
-
-void libererAVL(pUsine racine);
-
-void parcoursInfixeInverse(pUsine racine, FILE* fichier, const char* type);
-
-void traiterLigne(char* ligne, pUsine* racine);
-
-// Prototypes fonctions utilitaires de conversion
-
-long long parseLongLong(const char* str);
-double parseDouble(const char* str);
-
+// Fonctions de traitement (retournent 0 si succès, 1 si erreur)
+int mode_max(char* fichier_entree, char* fichier_sortie);
+int mode_src(char* fichier_entree, char* fichier_sortie);
+int mode_reel(char* fichier_entree, char* fichier_sortie);
 
 #endif
