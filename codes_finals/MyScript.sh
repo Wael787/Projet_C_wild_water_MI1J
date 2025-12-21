33e2f1d0faf53c393b725f3bbcc21244c1449254
Wael787 <wael.wadih06@gmail.com>
2025-12-21 12:39:09 +0100

    Update MyScript.sh

--- codes_finals/MyScript.sh
+++ codes_finals/MyScript.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 # version finale
-#date : 21/12/2025
-
+# date 21/12/2025
 
 # Vérification des Paramètres
 if [ $# -lt 2 ]; then
     echo "Usage :"
-    echo "  $0 <fichier.dat> histo {max|src|real|maxC|srcC|reelC}"
+    echo "  $0 <fichier.dat> histo {max|src|reel|maxC|srcC|reelC}"
     echo "  $0 <fichier.dat> leaks {id usine}"
     exit 1
 fi
@@ -17,8 +16,8 @@ dirname=$(dirname "$program")
 datafile="$dirname/dat/$1"
 action=$2
 
-start_time=$(date +%s)
-trap 'end_time=$(date +%s); echo -e "\nDurée totale du script : $((end_time - start_time)) s"' EXIT
+start_time=$(date +%s%3N)
+trap 'end_time=$(date +%s%3N); echo -e "\nDurée totale du script : $((end_time - start_time)) ms"' EXIT
 
 # Controle existence du fichier de données
 if [ ! -f "$datafile" ]; then
@@ -52,72 +51,90 @@ if [ "$action" = "histo" ]; then
     mode=$3
     csvfile="$dirname/csv/histo_$mode.csv"
 
-    # traitement sur le shell (max, src, real)
-    if [ "$mode" = "max" ] || [ "$mode" = "src" ] || [ "$mode" = "real" ]; then
+    # traitement sur le shell (max, src, reel)
+    if [ "$mode" = "max" ] || [ "$mode" = "src" ] || [ "$mode" = "reel" ]; then
         echo "Traitement via Shell/AWK ($mode)..."
         # Logique de calcul via AWK (simplifiée pour l'exemple)
         if [ "$mode" = "max" ]; then
-            awk -F';' '$1=="-" && $2!="-" && $3=="-" && $4!="-" {print $2";"$4}' "$datafile" | sort -t';' -k2,2nr > "$csvfile.tmp"
+            echo "=== Top 10 plus grandes usines ==="                       > $csvfile
+            echo "identifier;max volume(k.m3.year-1)"                       >> $csvfile
+            awk -F';' '$1=="-" && $2!="-" && $3=="-" && $4!="-" && $5=="-" {print $2";"$4}' $datafile | sort -t';' -k2,2gr | head -10       >> $csvfile
+            echo "=== Top 50 plus petites usines ==="                       >> $csvfile
+            echo "identifier;max volume(k.m3.year-1)"                       >> $csvfile
+            awk -F';' '$1=="-" && $2!="-" && $3=="-" && $4!="-" && $5=="-" {print $2";"$4}' $datafile | sort -t';' -k2,2g | head -50       >> $csvfile
         elif [ "$mode" = "src" ]; then
-            awk -F';' '$1=="-" && $2!="-" && $3!="-" && $4!="-" {sum[$3]+=$4} END {for (k in sum) print k";"sum[k]}' "$datafile" | sort -t';' -k2,2nr > "$csvfile.tmp"
+            echo "=== Top 10 plus grandes usines ==="                       > $csvfile
+            echo "identifier;src volume(k.m3.year-1)"                       >> $csvfile
+            awk -F';' '$1=="-" && $2!="-" && $3!="-" && $4!="-" {sum[$3]+=$4} END {for (k in sum) print k";"sum[k]}' "$datafile" | sort -t';' -k2,2nr |head -10  >> "$csvfile"
+            echo "=== Top 50 plus petites usines ==="                       >> $csvfile
+            echo "identifier;src volume(k.m3.year-1)"                       >> $csvfile
+            awk -F';' '$1=="-" && $2!="-" && $3!="-" && $4!="-" {sum[$3]+=$4} END {for (k in sum) print k";"sum[k]}' "$datafile" | sort -t';' -k2,2n |head -50  >> "$csvfile"
         else
-            awk -F';' '$1=="-" && $2!="-" && $3!="-" && $4!="-" {res=$4*(100-$5)/100; sum[$3]+=res} END {for (k in sum) print k";"sum[k]}' "$datafile" | sort -t';' -k2,2nr > "$csvfile.tmp"
+            echo "=== Top 10 plus grandes usines ==="                         > $csvfile
+            echo "identifier;reel volume(k.m3.year-1)"                       >> $csvfile
+            awk -F';' '$1=="-" && $2!="-" && $3!="-" && $4!="-" {res=$4*(100-$5)/100; sum[$3]+=res} END {for (k in sum) print k";"sum[k]}' "$datafile" | sort -t';' -k2,2nr |head -10 >> "$csvfile"
+            echo "=== Top 50 plus petites usines ==="                         >> $csvfile
+            echo "identifier;reel volume(k.m3.year-1)"                        >> $csvfile
+            awk -F';' '$1=="-" && $2!="-" && $3!="-" && $4!="-" {res=$4*(100-$5)/100; sum[$3]+=res} END {for (k in sum) print k";"sum[k]}' "$datafile" | sort -t';' -k2,2n |head -50 >> "$csvfile"            
         fi
+        
+        cat $csvfile
 
     # traitement sur le C (maxC, srcC, reelC) 
     elif [ "$mode" = "maxC" ] || [ "$mode" = "srcC" ] || [ "$mode" = "reelC" ]; then
         echo "Traitement via Programme C/AVL ($mode)..."
         compile "histo"
-        "$dirname/bin/histo" "$datafile" "$csvfile.tmp" "$mode"
+        csvfile=$dirname/csv/histo_$mode.csv
+        "$dirname/bin/$action" "$datafile" "$mode"
         retour=$?
         if [ $retour -ne 0 ]; then
             echo "Erreur : Le programme histo a échoué."
             exit 1
         fi
-        
-        # Si le fichier n'existe pas, le créer
-        if [ ! -f "$csvfile.tmp" ]; then
-            echo "identifier;volume" > "$csvfile.tmp"
-        fi
-    else
-        echo "Mode inconnu"; exit 1
-    fi
-
-    # top 10 max et top 50 min
-    echo "Génération des classements dans $csvfile..."
-    
-    echo "    TOP 10 PLUS GRANDS VOLUMES" > "$csvfile"
-    echo "identifier;volume" >> "$csvfile"
-    head -n 10 "$csvfile.tmp" >> "$csvfile"
-    
-    echo "" >> "$csvfile"
-    echo "    TOP 50 PLUS PETITS VOLUMES" >> "$csvfile"
-    echo "identifier;volume" >> "$csvfile"
-    tail -n 50 "$csvfile.tmp" | sort -t';' -k2,2n >> "$csvfile"
-
-    rm -f "$csvfile.tmp"
-
-    # GNUPLOT
-    if command -v gnuplot >/dev/null 2>&1; then
-        image_sortie="$dirname/csv/histo_$mode.png"
-        echo "Génération du graphique..."
-        # les 10 plus grandes 
-        gnuplot <<EOF
+        # GNUPLOT
+        if command -v gnuplot >/dev/null 2>&1; then
+            image_top10="$dirname/csv/histo_${mode}_$1_top10.png"
+            echo "Génération du graphique..."
+            # les 10 plus grandes 
+            gnuplot <<EOF
             set terminal pngcairo size 1000,600 font "Arial,10"
-            set output "$image_sortie"
+            set output "$image_top10"
             set datafile separator ";"
             set style fill solid
             set boxwidth 0.5
-            set xtics rotate by -45
+            set xtics rotate by -90
             set title "Top 10 - Volumes ($mode)"
             set ylabel "Volume (k.m3/an)"
             # On saute les lignes de texte du fichier pour ne tracer que les données
             plot "< grep -v '==' '$csvfile' | head -n 11 | tail -n 10" using 2:xtic(1) with boxes title "Volume"
 EOF
-        echo "Image générée : $image_sortie"
+            echo "Image générée : $image_top10"
+            image_top50=$dirname/csv/histo_${mode}_$1_top50.png
+            echo "Génération du graphique..."
+            # les 50 plus petites 
+            gnuplot <<EOF
+            set terminal pngcairo size 1000,600 font "Arial,10"
+            set output "$image_top50"
+            set datafile separator ";"
+            set style fill solid
+            set boxwidth 0.5
+            set xtics rotate by -90
+            set title "Top 50 - Volumes ($mode)"
+            set ylabel "Volume (k.m3/an)"
+            # On saute les lignes de texte du fichier pour ne tracer que les données
+            plot "< awk '/Top 50/ {flag=1} flag' '$csvfile' " using 2:xtic(1) with boxes title "Volume"
+EOF
+            echo "Image générée : $image_top50"
+        fi
+
+        cat "$csvfile"
+        cp $csvfile /drives/d/wildwater
+        cp $image_top10 /drives/d/wildwater 
+        cp $image_top50 /drives/d/wildwater
+    else
+        echo "Mode inconnu"; exit 1
     fi
 
-    cat "$csvfile"
 
 # Action = leaks 
 elif [ "$action" = "leaks" ]; then
